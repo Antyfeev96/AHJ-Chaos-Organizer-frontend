d9270187bf45644099d5492e0f1ce73ad79e263e
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-04-22 13:53:12 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  target: 'web',
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin({}),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({ filename: 'main.css' }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new ESLintPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './src', to: './' },
+      ],
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /service.worker\.js$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  devServer: {
+    host: 'localhost',
+    open: true,
+    contentBase: path.resolve(__dirname, './src/index.js'),
+    watchContentBase: true,
+  },
+};
