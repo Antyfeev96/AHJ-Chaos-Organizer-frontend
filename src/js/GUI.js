42d1134045dd487a78537728168c40e30849b720
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-04-27 10:49:46 +0300

    Trash commented

--- src/js/GUI.js
+++ src/js/GUI.js
@@ -1,2 +1,2 @@
 // Photo by <a href="https://unsplash.com/@xcrap?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">César Couto</a> on <a href="https://unsplash.com/s/photos/background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */
